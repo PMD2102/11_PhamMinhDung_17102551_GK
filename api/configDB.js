b085356f4974444e0446639e6bdf4d137dfd8570
PMD2102 <phamminhdung0102@gmail.com>
2020-11-07 07:44:37 +0700

    ok

---
+++ api/configDB.js
@@ -0,0 +1,10 @@
+module.exports = {
+    aws_table_name: 'sanpham',
+    aws_local_config: {
+    },
+    aws_remote_config: {
+      accessKeyId: 'AKIAIBCQ4Q4GYN352L4Q',
+      secretAccessKey: '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',
+      region: 'us-east-2',
+    }
+};
